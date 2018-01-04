d89e929def0a5ebce1eab569a1f454850dc96e5f
Diego <diegomayorga86@hotmail.com>
2018-01-04 17:14:09 -0300

    feat(jquery): add new exercises for jquery module

---
+++ Clase 4/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+	 /*La función .fadeOut()  hace desaparecer 
+	 el elemento seleccionado con  la funcion jQuery "$".
+	 El parámetro "slow" determina la velocidad en la cual ocurre 
+	 la desaparición. */
+	$("div").fadeOut("slow");
+
+	/* La función .fadeIn() hace apaarecer el elemento seleccionado*/ 
+   	$("div").fadeIn("slow");
+
+});
---
+++ Selectores_consigna/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  var $parrafoQueDesaparece = $("#parrafoQueDesaparece");
+  $parrafoQueDesaparece.fadeOut();
+})
