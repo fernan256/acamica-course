78b6a8eb75493520fbd39d19446ab33b0885cc2e
Diego <diegomayorga86@hotmail.com>
2018-01-18 17:32:20 -0300

    feat(pixel-art): make some changes on exercise

--- pixel-art/js/pixel-art.js
+++ pixel-art/js/pixel-art.js
@@ -20,6 +20,8 @@ var nombreColores = ['White', 'LightYellow',
   'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
 ];
 
+var $paleta = $('#paleta');
+var $grillaDePixeles = $('#grilla-pixeles');
 // Variable jQuery para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
 var $colorPersonalizado = $('#color-personalizado');
@@ -28,5 +30,62 @@ $colorPersonalizado.change(function() {
   // Se guarda el color de la rueda en colorActual
   var colorActual = $colorPersonalizado.val();
   // Completar para que cambie el indicador-de-color al colorActual
-
+  $('#color-personalizado').css('background-color', $backgroundColor);
 });
+
+
+function generarPaleta(indexElemento, color) {
+    var $divColor = $('<div>', {'class': 'color-paleta', 'style': 'background-color: ' + color + ";'"});
+    $paleta.append($divColor);
+}
+
+$.each(nombreColores, generarPaleta);
+
+// 4 - Creando pixeles de la grilla
+
+var pixelesDeGrilla = [];
+
+for (var i = 0; pixelesDeGrilla.length < 1750; i++) {
+  pixelesDeGrilla[i] = i;
+}
+
+function agregarPixelDeGrilla(indexElemento, pixel) {
+  var $divPixel = $('<div/>');
+  $grillaDePixeles.append($divPixel);
+}
+
+// 5 - Recorrer elementos de la pixelesDeGrilla con la función .each()
+
+$.each(pixelesDeGrilla, agregarPixelDeGrilla);
+
+
+function cambiarColorIndicador() {
+  var $backgroundColor = $(this).css('background-color');
+  $('#indicador-de-color').css('background-color', $backgroundColor);
+}
+
+$('.color-paleta').click(cambiarColorIndicador);
+
+var $divPixelGrilla = $('#grilla-pixeles.cursor-personalizado div');
+
+function cambiarColor() {
+  var $indicadorDeColor = $('#indicador-de-color').css('background-color');
+  $(this).css('background-color', $indicadorDeColor);
+}
+
+
+$divPixelGrilla.click(cambiarColor);
+
+function detectarBoton (event) {
+  console.log(event);
+  if(event && event.button==0){
+    alert("derecho");
+  } else if(event && event.button==1) {
+    alert("medio");
+  }
+  else {
+    alert("izquierdo");
+  }
+}
+
+detectarBoton();
